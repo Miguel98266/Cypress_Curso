824e3dfd10af8d3cdee998ac29fa20c070f8b75c
Miguel Calixto <miguelcalixto@wolfsellers.com>
2023-06-27 13:26:17 -0600

    First commit

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: 'xf56en',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      
+    },
+    specPattern:"cypress/e2e/automation-store/*.{js,jsx,ts,tsx,feature}"  
+  },
+  
+});
---
+++ cypress/e2e/automation-store/contact-us.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+describe("Test Contact Us form via Automation Test Store", () => {
+    it("Should be able to submit a successful submission via contact us form", () => {
+        cy.visit("https://www.automationteststore.com/");
+        // $ search the word in the last sentence
+        // ^ search at the begining of the sentence
+        cy.get("a[href$='contact']").click();
+        // cy.xpath("//a[contains(@href, 'contact')]").click();
+        cy.get('#ContactUsFrm_first_name').type("Joe");
+        cy.get('#ContactUsFrm_email').type("joe_blogs123@gmail.com");
+        cy.xpath('//textarea[@id="ContactUsFrm_enquiry"]').type("Do you provide additional discount on bulk orders?")
+        cy.get('button[title="Submit"]').click();
+    });
+})
