93fd2f9c83b2e0e8d1f46f62dd3de0227de1f388
Miguel Calixto <miguelcalixto@wolfsellers.com>
2023-06-27 16:49:53 -0600

    Example selectors

---
+++ cypress/e2e/automation-store/store.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+describe("Selector examples", ()=>{
+    it("Examples of Selectors via WebdriverUni Contact Us Page",()=>{
+        cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html")
+
+        // By tag name
+        cy.get("input")
+        // By attribute name and value
+        cy.get("input[name='first_name']")
+
+        //By id
+        cy.get("#contact_me")
+
+        // By class
+        cy.get(".feedback-input")
+
+        //By multiple-class
+        cy.get("[class='navbar navbar-inverse navbar-fixed-top']")
+
+        //By two attributes
+        cy.get("[name='email'][placeholder='Email Address']")
+
+        //By xpath
+        cy.xpath("//input[@name='first_name']")
+    })
+})
