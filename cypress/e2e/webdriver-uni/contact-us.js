965b9583a87ca2f4838d72f39e8282914007c4a5
Miguel Calixto <miguel98266@gmail.com>
2023-06-27 23:04:14 -0600

    Fixes

---
+++ cypress/e2e/webdriver-uni/contact-us.js
@@ -0,0 +1,26 @@
+// reference types="Cypress" 
+
+
+
+describe('Test Contact Us form via webdriver Uni', () => { 
+    it("Should be able to submit a successful submission via contact us form",()=>{
+        // cypress code
+        cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html")
+        // cy.get('#contact-us').click()
+        cy.get('[name="first_name"]').type("Miguel")
+        cy.get('[name="last_name"]').type("Calixto")
+        cy.get('[name="email"]').type("miguel@gmail.com")
+        cy.get('textarea.feedback-input').type("comment")
+        cy.get('[type="submit"]').click()
+    })
+    it.only("Should not be able to submit a successful submission via contact us form as all fieds are required",()=>{
+        // cypress code
+        cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html")
+        // cy.get('#contact-us').click()
+        cy.get('[name="first_name"]').type("Angel")
+        cy.get('[name="last_name"]').type("Serrano")
+        
+        cy.get('textarea.feedback-input').type("comment")
+        cy.get('[type="submit"]').click()
+    })
+ })
